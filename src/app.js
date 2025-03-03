984627ad4af1698e5c16a4a14c2b3e7d0dc639fa
Marcos Garcia <86496161+MarcosDemian@users.noreply.github.com>
2025-03-02 22:27:59 -0300

    change url app

--- src/app.js
+++ src/app.js
@@ -2,17 +2,19 @@ import express from 'express';
 import cors from 'cors';
 import characterRoutes from './routes/characters.routes.js';
 import { config } from 'dotenv';
-config()
+config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const url = 'https://solodleapi.up.railway.app/api/characters'
+const baseUrl = 'api/characters'; // Ruta base para las rutas de personajes
 
 app.use(cors());
 app.use(express.json());
 
-app.use(url, characterRoutes);
+// Usa la ruta base para las rutas de personajes
+app.use(baseUrl, characterRoutes);
 
-app.listen(port, url, () => {
-  console.log(`Servidor corriendo en ${url}`);
+// Inicia el servidor
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
