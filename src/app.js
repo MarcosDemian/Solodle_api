f2081befb19dcb39985606194eb07a4edc5eb1a7
Marcos Garcia <86496161+MarcosDemian@users.noreply.github.com>
2025-03-10 17:58:31 -0300

    config CORS

--- src/app.js
+++ src/app.js
@@ -8,17 +8,32 @@ const app = express();
 const port = process.env.PORT || 3000;
 const baseUrl = '/api/characters'; // Ruta base para las rutas de personajes
 
-app.use(cors({
-  origin: 'https://solodle.netlify.app',
-  methods: ['GET', 'POST'],
-  credentials: true,
-}));
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  if (origin === 'http://localhost:3000') {
+    cors({
+      origin: origin,
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+      credentials: true,
+    })(req, res, next);
+  }
+  else if (origin === 'https://solodle.netlify.app') {
+    cors({
+      origin: origin,
+      methods: ['GET', 'POST'],
+      credentials: true,
+    })(req, res, next);
+  }
+  else {
+    res.status(403).json({ error: 'Acceso no permitido desde este origen' });
+  }
+});
+
 app.use(express.json());
 
-// Usa la ruta base para las rutas de personajes
 app.use(baseUrl, characterRoutes);
 
-// Inicia el servidor
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
